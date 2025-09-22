414d60f45d0164c11179d2cfbc6ed3a4244fad36
rik305 <riktt5@gmail.com>
2025-09-21 23:20:12 -0400

    Add LeetCode solution - 2025-09-21 23:20:12

--- solutions.py
+++ solutions.py
@@ -75,3 +75,22 @@ LEFT JOIN Address a ON p.personId = a.personId;
 # End of solution for Problem 175
 ==================================================
 
+
+# Problem 176: Second Highest Salary
+# URL: https://leetcode.com/problems/second-highest-salary/
+# Date: 2025-09-21 23:20:12
+
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame(columns = ['SecondHighestSalary'])
+    salaries = list(employee['salary'].unique())
+    if len(salaries) == 1:
+        df.loc[0] = None
+        return df
+    else:
+        df.loc[0] = ((sorted(salaries))[1])
+
+
+# End of solution for Problem 176
+==================================================
