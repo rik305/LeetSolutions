30cf3c5e4f2d355055c5aa8d2f651086d112fd50
rik305 <riktt5@gmail.com>
2026-06-30 08:56:19 -0600

    Add LeetCode solution - 2026-06-30 08:56:19

--- solutions.py
+++ solutions.py
@@ -122,9 +122,10 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
 
 
 
+
 # Problem 1460: Number of Substrings Containing All Three Characters
 # URL: https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
-# Date: 2026-06-30 08:55:41
+# Date: 2026-06-30 08:56:19
 
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
